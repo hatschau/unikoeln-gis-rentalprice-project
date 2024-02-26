7982745e5484238712f77593bcedae82570f0412
Helena <helenakoester@gmx.de>
2024-02-26 14:40:42 +0100

    Clean up css

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -2,17 +2,16 @@ import styled from 'styled-components'
 
 const MenuItem = styled.button`
 width: 100%;
-border-radius: none;
-background: ${props => props.$styleprops ? $styleprops.background : "#000000"};
+background: inherit;
 `
 
 const MenuContainer = styled.div`
 position: sticky;
 display: flex;
-justify-content: space-between;
 width: 100%;
 height: 3rem;
 background: #000000;
+z-index: 999;
 `
 
 export default function Menu({ menuItems, scrollToSection }, styleprops) {
@@ -20,13 +19,13 @@ export default function Menu({ menuItems, scrollToSection }, styleprops) {
     console.log(styleprops)
 
     return (
-        <>
-            <MenuContainer>
-                {menuItems.map((menuItem, index) => (
-                    <MenuItem key={index} styleprops={styleprops} onClick={() => scrollToSection(menuItem)}>{menuItem} </MenuItem>
-                ))}
-            </MenuContainer>
 
-        </>
+        <MenuContainer>
+            {menuItems.map((menuItem, index) => (
+                <MenuItem key={index} $styleprops={styleprops} onClick={() => scrollToSection(menuItem)}>{menuItem} </MenuItem>
+            ))}
+        </MenuContainer>
+
+
     )
 }
